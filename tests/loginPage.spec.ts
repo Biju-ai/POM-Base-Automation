5d8be09e2b05c98b9c94b6c1f9effcf2b5ba5068
Biju-maker-dev <biju.shrestha@anchorpoints.io>
2026-08-10 14:30:35 +0545

    jfvb

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -10,6 +10,6 @@ test.describe("valid login function", () =>{
         await loginPage.verifyLoginSuccess();
     })
     test("second test",async({page}) =>{
-        console.log("hello everyone rascallgit ")
+        console.log("hello rascallgit ")
     });
 })
